a734c8ca608e07d3c54326b466be0bce9a2853e5
mushizoulu <139825649+mushizoulu@users.noreply.github.com>
2026-05-28 21:47:02 +0900

    Add files via upload

--- content/works/new-releases.js
+++ content/works/new-releases.js
@@ -10,29 +10,30 @@ window.siteContentChunks.push({
       title: "【繁體中文版】願光降臨系列Ep.1『月音漸盈』 ",
       meta: ["繁體中文版", "ASMR", "RJ01614308"],
       copy: "【願光降臨系列】這是一個兩名一度幾乎放棄音樂的少女，因彼此的音色而再次找回希望之光的故事。心中懷抱著巨大缺口的她們，也隨著音色的完成而重新綻放出光輝，並一步步走向圓滿。本廣播劇描繪的正是這樣一段故事。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01614308.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01614308.html/?locale=zh_TW"
     },
     {
       tag: "ASMR",
       title: "【繁體中文版】願光降臨系列Ep.2『星之華彩』 ",
       meta: ["繁體中文版", "ASMR", "RJ01614910"],
       copy: "【願光降臨系列】這是一個兩名一度幾乎放棄音樂的少女，因彼此的音色而再次找回希望之光的故事。心中懷抱著巨大缺口的她們，也隨著音色的完成而重新綻放出光輝，並一步步走向圓滿。本廣播劇描繪的正是這樣一段故事。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01614910.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01614910.html/?locale=zh_TW"
     },
     {
       tag: "ASMR",
       title: "【繁體中文版】【社團作品特別特價中‼️】【CV：富田美憂／和泉風花】與你一同走進童話世界的《小紅帽》療癒系ASMR【格林童話連結】",
       meta: ["繁體中文版", "ASMR", "RJ01590646"],
       copy: "這是第6620人的故事，也是屬於你的故事。你突然誤闖了「開幕之地」。「看起來這次好像稍微有點『不一樣』呢……不過嘛，其實我也不是很清楚……」那位自稱「牧羊人」的人，隨心所欲地說著自己的事，卻什麼也沒有解釋，便將玩家（也就是你）送往了《小紅帽》的世界。在那裡，溫柔的梅茲會療癒疲憊的你。與梅茲一起度過的溫柔日常，將成為一個能夠療癒身心、讓人放鬆下來的空間。她還會讓你枕在她的大腿上，為你輕輕掏耳。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01590646.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01590646.html/?locale=zh_TW"
     },
     {
       tag: "ASMR",
       title: "【繁體中文版】【CV:飯田ヒカル】隔壁那位可愛的大姊姊，其實是有點少根筋的兔女郎",
       meta: ["繁體中文版", "ASMR", "RJ01593938"],
       copy: "歡迎來到 BUNNY LOUNGE MIMOSA。這裡是一間專為稍感疲憊的夜晚而設的會員制兔女郎 Lounge。在店內，兔女郎們將以美酒、交談，以及「稍微近一些的距離」，溫柔陪伴你的時光。這裡沒有華麗喧鬧的表演，有的只是讓人在下班後得以卸下緊繃的靜謐空間，還有與坐在身旁的兔女郎共度的，那段平凡卻令人放鬆的片刻。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01593938.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01593938.html/?locale=zh_TW"
     },
+    /*
     {
       tag: "ASMR",
       title: "【简体中文版】和姐姐开启禁断的性生活！甜蜜气息令我心乱♪",
@@ -54,68 +55,69 @@ window.siteContentChunks.push({
       copy: "普通少年×人外觸手外星人美少女的Boy Meets Girl(純愛)作品。觸手主要是用在少年身上。是以 2000 年代的世界系、輕小說感為目標所創作的作品。",
       link: "https://www.dlsite.com/home/work/=/product_id/RJ01588720.html"
     },
+    */
     {
       tag: "ASMR",
       title: "【繁體中文版】放學後約會『與女朋友的絕對保密的戀愛物語』系列～文靜而有些纖弱的青梅竹馬，御池櫻波～【CV.天音ゆかり】",
       meta: ["繁體中文版", "ASMR", "RJ01617623"],
       copy: "在入睡前，要不要讓深深喜歡著你的女孩子們陪伴你享受一段療癒時光呢？從經典的掏耳朵到梵天，再加上本社團大受好評、非常講究的耳邊吹氣，還有安眠必備的陪睡，都能讓你盡情享受。在本系列中，你可以體驗與魅力十足的女孩子展開酸酸甜甜、絕對保密的放學後約會感，還請盡情沉浸在這段療癒時光之中。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01617623.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01617623.html/?locale=zh_TW"
     },
     {
       tag: "ASMR",
       title: "【简体中文版】耳边疗愈小店『欢迎光临喵喵猫猫风流庵！』～充满母性的大姐姐猫娘椿，想要尽情宠爱主人！～【CV.直田姫奈】",
       meta: ["简体中文版", "ASMR", "RJ01610239"],
       copy: "在休息时，不妨和充满母性的大姐姐猫娘椿一起，度过一段悠闲而温柔的治愈时光吧！经典的掏耳、梵天，以及耳边吹气当然一样不少。此外，在本系列中，你还可以尽情享受与各种不同类型的猫娘相伴的放松片刻。请好好享受这份几乎让人必定安心入睡的治愈体验。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01610239.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_CN/id/RJ01610239.html/?locale=zh_CN"
     },
     {
       tag: "ASMR",
       title: "【简体中文版】【助眠ASMR】献给想一边被爱一边入睡的你✿甜蜜陪睡，极致放松✨温柔治愈疲惫的心♪零距离耳语、亲密触碰、彼此相爱✨带着幸福的心情进入梦乡的晚安ASMR",
       meta: ["简体中文版", "ASMR", "RJ01589133"],
       copy: "温柔、温暖、治愈、让人安心……零距离★贴在一起♪甜甜的助眠ASMR✨在睡前聊聊天、轻轻触碰彼此、在耳边低声说着「喜欢你」、互相依偎、彼此相爱……这是只属于两个人的、幸福的夜晚时光✨一定能带着满满的幸福感入睡。甜甜的、极致放松♪沉浸式陪睡体验✨",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01589133.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_CN/id/RJ01589133.html/?locale=zh_CN"
     },
     {
       tag: "ASMR",
       title: "【简体中文版】兽耳乌托邦《疗愈心灵亭 八百万》系列～温婉娴静的猫神姐姐，猫又永远～【CV：井上遥乃】",
       meta: ["简体中文版", "ASMR", "RJ01590641"],
       copy: "要不要和长着兽耳、想把你从头到尾都温柔治愈一遍的神明们，一起度过一段安宁放松的时光呢？除了经典的掏耳与梵天，以及绝不能缺席、精心设计的耳边吹气，以及与神明共享的甜蜜混浴和陪睡时光。在这个系列中，你可以体验到仿佛梦境一般、与兽耳神明们相伴的美好时刻。还请尽情享受这段疗愈身心的时光。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01590641.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_CN/id/RJ01590641.html/?locale=zh_CN"
     },
     {
       tag: "ASMR",
       title: "【繁體中文版】獸耳烏托邦《心靈療癒亭 八百萬》系列～嫻靜溫婉的貓神大姊姊，貓又永遠～【CV：井上遥乃】",
       meta: ["繁體中文版", "ASMR", "RJ01590643"],
-      copy: "想不想與那些長著獸耳、滿懷溫柔、只為將你徹底療癒的神明們，一同度過一段寧靜而甜美的時光呢？本作除了收錄經典的掏耳與梵天療癒之外，以及絕對不可或缺、精心設計的耳邊輕吹，更有與神明共享的甜蜜混浴，以及令人安心沉醉的陪睡時光。在這個系列中，你將能夠體驗到彷彿夢境一般、與獸耳神明們相依相伴的特別片刻。還請盡情享受這段足以溫柔撫慰身心的療癒時光。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01590643.html"
+      copy: "想不想與那些長著獸耳、滿懷溫柔、只為將你徹底療癒的神明們，一同度過一段寧靜而甜美的時光呢？本作除了收錄經典的掏耳與梵天療癒之外，以及絕對不可或缺、精心設計的耳邊輕吹，更有與神明共享的甜蜜混浴，以及令人安心沉醉的陪睡時光。在這個系列中，你將能夠體驗到彷彿夢境一般、與獸耳神明们相依相伴的特別片刻。還請盡情享受這段足以溫柔撫慰身心的療癒時光。",
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01590643.html/?locale=zh_TW"
     },
     {
       tag: "ASMR",
       title: "【繁體中文版】【睡眠導入ASMR】給想要邊被疼愛邊睡著的你✿用甜蜜陪睡極度放鬆✨溫柔療癒疲憊的心靈♪零距離低語、互相碰觸、相愛✨能以幸福的心情睡著的晚安ASMR ",
       meta: ["繁體中文版", "ASMR", "RJ01588548"],
       copy: "溫柔又溫暖且讓人感到療癒愜意…。零距離★緊貼♪甜蜜睡眠導入ASMR✨在睡前聊天、互相碰觸、互相說著「喜歡」、相愛…那樣兩人獨處的幸福夜晚時間✨一定能用幸福的心情入睡。極度甜蜜♪放鬆陪睡體驗✨",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01588548.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01588548.html/?locale=zh_TW"
     },
     {
       tag: "ASMR",
       title: "【繁體中文版】獸耳樂園《心靈療癒亭八百萬》系列～毫無距離感、天真爛漫的戌神，犬神小狛～【CV.湊みや】 ",
       meta: ["繁體中文版", "ASMR", "RJ01600964"],
       copy: "想不想在入睡前，和想徹底療癒你的獸耳神明們一起度過一段溫馨放鬆的時光呢？除了經典的掏耳與梵天，還有不可或缺的耳邊吹氣，以及和神明甜甜蜜蜜的共浴與陪睡。在這個系列中，你將能體驗與獸耳神明共度的夢幻時光，還請盡情享受這段撫慰身心的療癒片刻。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01600964.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01600964.html/?locale=zh_TW"
     },
     {
       tag: "ASMR",
       title: "【繁體中文版】耳畔療癒處『歡迎來到喵喵貓風雅庵喵』～有點我行我素又愛吃醋的蓮音想獨占主人！～【CV.結名美月】 ",
       meta: ["繁體中文版", "ASMR", "RJ01605109"],
       copy: "作為陪伴你入睡的時光，要不要和有點我行我素的貓咪女孩蓮音一起度過一段軟綿綿的療癒時光呢？經典的掏耳、梵天，還有輕輕朝耳邊呼氣當然都少不了，在這個系列裡，你還能享受與各種類型貓娘共度的悠閒片刻。請盡情享受這份讓人安心睡著的療癒時光。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01605109.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01605109.html/?locale=zh_TW"
     },
     {
       tag: "ASMR",
       title: "【繁體中文版】戀人ASMR～愛撒嬌的小狗系戀人的貼心呵護～【CV.涼本あきほ】 ",
       meta: ["繁體中文版", "ASMR", "RJ01613543"],
       copy: "這次的女主角，是和你同歲的表妹，卻又有一種比你小幾歲、如小狗一樣黏人的女孩子的感覺。她總是陪在你身邊，最喜歡的就是你身上的氣味。不過，她也覺得不能一直維持現在這樣的關係，於是主動向你發起一波又一波甜蜜攻勢——這是一部充滿愛意、療癒感加倍再加倍的 ASMR。",
-      link: "https://www.dlsite.com/home/work/=/product_id/RJ01613543.html"
+      link: "https://dlaf.jp/home/dlaf/=/t/s/link/work/aid/pssitewww/locale/zh_TW/id/RJ01613543.html/?locale=zh_TW"
     }
   ]
 });
