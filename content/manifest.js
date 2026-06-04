db54333821a2ae4d654a650e56ede58c834ce764
mushizoulu <139825649+mushizoulu@users.noreply.github.com>
2026-06-04 10:33:43 +0900

    Add files via upload

--- content/manifest.js
+++ content/manifest.js
@@ -2,7 +2,9 @@ window.siteContentFiles = [
   "content/works/new-releases.js",
   "content/posts/posts-20260528.js",
   "content/posts/posts-20260530.js",
+  "content/posts/posts-20260604.js",
   "content/picks/picks-20260528.js"
+  
 ];
 
 window.siteContentChunks = window.siteContentChunks || [];
